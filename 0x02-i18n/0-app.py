c057a724e04099506d07e64a32148b5104836c0d
shahendamagdy97 <shahendamagdy97@yahoo.com>
2024-05-29 00:01:03 +0300

    Modification

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
+"""A Basic Flask app.
 """
-0-app.py
-"""
-
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello_world() -> str:
-    """Return a simple string as our response"""
-    return render_template("0-index.html")
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
